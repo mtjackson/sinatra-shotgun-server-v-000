d47e3e7f21ee47712a1414d90e725b469dc59b45
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-15 14:31:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,9 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do
-    "Welcome to your app!!!! I BUILT THIS!"
-  end
+#  get '/' do
+#    "Welcome to your app!!!! I BUILT THIS!"
+#  end
 
   get '/' do
     "Started my server using Shotgun!"
